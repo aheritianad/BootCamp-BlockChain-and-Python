293d4fde27bb2691c377cbbec35e856f2972bf8c
Heritiana Daniel Andriasolofo <aheritianad@gmail.com>
2023-05-01 12:57:37 +0300

    Update: setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup
 
-with open("bcbcpy/readme.md") as f:
+with open("src/bcbcpy/readme.md") as f:
     long_description = f.read()
+with open("LICENSE") as f:
+    license = f.read()
 
 setup(
     name="BootCamp-BlockChain-and-Python",
-    description="",
+    description="A standalone crypto & blockchain demo package on python.",
     long_description=long_description,
     author="Heritiana Daniel, Andriasolofo",
     author_email="aheritianad@gmail.com",
+    url="https://github.com/aheritiana/BootCamp-BlockChain-and-Python/",
     download_url="https://github.com/aheritiana/BootCamp-BlockChain-and-Python/",
     packages=["bcbcpy"],
     package_dir={"bcbcpy": "src/bcbcpy"},
+    keywords=[
+        "cryptography",
+        "blockchain",
+        "hash",
+    ],
+    license=license,
 )
