1c1a6c5bfca2f52970018dde2c383d83270305d4
Heritiana Daniel Andriasolofo <aheritianad@gmail.com>
2023-05-01 06:33:59 +0300

    Add: typing for blockchain

---
+++ bcbcpy/blockchain/typings.py
@@ -0,0 +1,6 @@
+from bcbcpy import __author__
+
+
+from bcbcpy.blockchain.block.block import Block, InitialBlock, MultipleBlocks
+from bcbcpy.blockchain.block.transaction import TransactionBlock, TransactionData
+from bcbcpy.blockchain.chain import Chain, RootChain
