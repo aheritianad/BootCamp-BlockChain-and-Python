1ec2d59d080e803628b0d06aca5345b26bfbdad5
Heritiana Daniel Andriasolofo <aheritianad@gmail.com>
2023-05-12 20:10:35 +0300

    update: setup

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
-from setuptools import setup, find_packages
+import setuptools
 
 with open("src/bcbcpy/readme.md") as f:
     long_description = f.read()
+
 with open("LICENSE") as f:
     license = f.read()
 
-setup(
-    name="BootCamp-BlockChain-and-Python",
+setuptools.setup(
+    name="bcbcpy",
     version="1.0.0dev1",
-    description="A standalone crypto & blockchain demo package on python.",
+    description="A standalone(no-third-party) crypto & blockchain demo package on python.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Heritiana Daniel, Andriasolofo",
     author_email="aheritianad@gmail.com",
-    url="https://github.com/aheritiana/BootCamp-BlockChain-and-Python/",
-    download_url="https://github.com/aheritiana/BootCamp-BlockChain-and-Python/",
-    packages=["bcbcpy"],
-    package_dir={
-        "bcbcpy": "src/bcbcpy",
+    project_urls={
+        "Source": "https://github.com/aheritiana/BootCamp-BlockChain-and-Python/",
     },
+    url="https://github.com/aheritiana/BootCamp-BlockChain-and-Python/",
+    packages=setuptools.find_packages(
+        include=(
+            "bcbcpy",
+            "bcbcpy.*",
+        )
+    ),
+    package_dir={"bcbcpy": "bcbcpy"},
     keywords=[
         "cryptography",
         "blockchain",
         "hash",
     ],
     license=license,
+    python_requires=">=3.9",
+    include_dirs=["tuto"],
+    package_data={"tuto": ["tuto.ipynb"]},
 )
